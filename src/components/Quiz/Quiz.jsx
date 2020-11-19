33654df4bfaf3baa2ef6950a9898c57ab18f675c
Sophie Roe <sophie.roe@Nology.local>
2020-11-19 14:38:58 +0000

    formatting of document

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -6,15 +6,15 @@ const Quiz = () => {
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => console.log(data);
   console.log(errors);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
       <label>What is the best food?</label>
-      <input type="answer" placeholder="answer" name="answer" ref={register({required: true, pattern: { value:/pizza/i, message:"has to be pizza",}})} />
+      <input type="answer" placeholder="answer" name="answer" ref={register({ required: true, pattern: { value: /pizza/i, message: "has to be pizza", } })} />
       {errors.answer && errors.answer.type === "required" && <span>This is required</span>}
       {errors.answer && errors.answer.type === "pattern" && <span>It has to be pizza!</span>}
-      <input type="submit"/>
-      </form>
+      <input type="submit" />
+    </form>
   );
 }
 
--- src/components/WeatherAppMain/WeatherAppMain.jsx
+++ src/components/WeatherAppMain/WeatherAppMain.jsx
@@ -1,70 +1,70 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./WeatherAppMain.module.scss";
 
 const WeatherAppMain = () => {
 
-  const [weather,setWeather] = useState([])
-  const [bristol,setBristol] = useState([])
-  const [rome,setRome] = useState([])
+  const [weather, setWeather] = useState([])
+  const [bristol, setBristol] = useState([])
+  const [rome, setRome] = useState([])
 
-  const fetchData =() => {
+  const fetchData = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=2643744&units=metric&appid=8db2109807015fdef60439fd9eb32637`)
-    .then(response => response.json())
-    .then(response => {
-      setWeather(response.main)
-    });
+      .then(response => response.json())
+      .then(response => {
+        setWeather(response.main)
+      });
   }
 
-    useEffect(() => {
+  useEffect(() => {
     fetchData();
   }, [])
 
-  const fetchBristol =() => {
+  const fetchBristol = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=4749005&units=metric&appid=8db2109807015fdef60439fd9eb32637`)
-    .then(response => response.json())
-    .then(response => {
-      setBristol(response.main)
-    });
+      .then(response => response.json())
+      .then(response => {
+        setBristol(response.main)
+      });
   }
 
-  const fetchRome =() => {
+  const fetchRome = () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=4219762&units=metric&appid=8db2109807015fdef60439fd9eb32637`)
-    .then(response => response.json())
-    .then(response => {
-      setRome(response.main)
-    });
+      .then(response => response.json())
+      .then(response => {
+        setRome(response.main)
+      });
   }
 
   return (
 
     <div>
       <section className={styles.main}>
-      <section className={styles.card}>
-      <h1>London</h1>
-      <div>Temp: {weather.temp} °C</div>
-   <div>Min Temp: {weather.temp_min} °C</div>
-   <div>Max Temp: {weather.temp_max} °C</div>
-   <div>Humidity: {weather.humidity} %</div>
-   </section>
-   <section className={styles.card}>
-   <h1>Bristol</h1>
-   <button onClick={fetchBristol}>Click for Bristol Weather!</button>
-      <div>Temp: {bristol.temp} °C</div>
-   <div>Min Temp: {bristol.temp_min} °C</div>
-   <div>Max Temp: {bristol.temp_max} °C</div>
-   <div>Humidity: {bristol.humidity} %</div>
-   </section>
-   <section className={styles.card}>
-   <h1>Rome</h1>
-   <button onClick={fetchRome}>Click for Rome Weather!</button>
-      <div>Temp: {rome.temp} °C</div>
-   <div>Min Temp: {rome.temp_min} °C</div>
-   <div>Max Temp: {rome.temp_max} °C</div>
-   <div>Humidity: {rome.humidity} %</div>
-   </section>
-   </section>
-   </div>
-  
+        <section className={styles.card}>
+          <h1>London</h1>
+          <div>Temp: {weather.temp} °C</div>
+          <div>Min Temp: {weather.temp_min} °C</div>
+          <div>Max Temp: {weather.temp_max} °C</div>
+          <div>Humidity: {weather.humidity} %</div>
+        </section>
+        <section className={styles.card}>
+          <h1>Bristol</h1>
+          <button onClick={fetchBristol}>Click for Bristol Weather!</button>
+          <div>Temp: {bristol.temp} °C</div>
+          <div>Min Temp: {bristol.temp_min} °C</div>
+          <div>Max Temp: {bristol.temp_max} °C</div>
+          <div>Humidity: {bristol.humidity} %</div>
+        </section>
+        <section className={styles.card}>
+          <h1>Rome</h1>
+          <button onClick={fetchRome}>Click for Rome Weather!</button>
+          <div>Temp: {rome.temp} °C</div>
+          <div>Min Temp: {rome.temp_min} °C</div>
+          <div>Max Temp: {rome.temp_max} °C</div>
+          <div>Humidity: {rome.humidity} %</div>
+        </section>
+      </section>
+    </div>
+
   );
 };
 
